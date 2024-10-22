c237013145e2ce4909fc16b10a17d448a1413750
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2024-10-22 14:42:56 +0700

    empty message

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,46 @@
+<?php
+
+require 'config.php';
+
+try {
+    $query = $conn->prepare("SELECT * FROM Task");
+
+} catch (mysqli_sql_exception $e) {
+    die("" . $e->getMessage());
+}
+?>
+
 <html>
 
+<head>
+    <title>Task Management</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
 <body>
-    <h1>Selamat, Anda berhasil login</h1>
-    <p><a href="index.php">log out</a></p>
+    <div class="container">
+        <nav>
+            <img src="res/Report Card.png" id="logo">
+            <a href="index.php" id="logout"><img src="res/Emergency Exit.png" alt=""></a>
+        </nav>
+        <div>
+            <h1>A simple Task Manager</h1>
+            <div class="card">
+                <?php
+                $query = $conn->prepare("SELECT * FROM Task");
+                $query->execute();
+                $row = $query->fetchall(PDO::FETCH_ASSOC);
+                $rowCount = count($row);
+                if ($rowCount == 0) { ?>
+                    <div class="empty">
+                        <h2>You Dont have any task</h2>
+                        <img src="res/Nothing Found.png">
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
